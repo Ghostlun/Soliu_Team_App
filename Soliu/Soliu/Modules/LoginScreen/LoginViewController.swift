40c6e21e529c905c16345d70e5f70cde7297f391
Ghostlun <40644178+Ghostlun@users.noreply.github.com>
2021-09-07 20:29:00 -0500

    Test updates
    Test updates

--- Soliu/Soliu/Modules/LoginScreen/LoginViewController.swift
+++ Soliu/Soliu/Modules/LoginScreen/LoginViewController.swift
@@ -50,5 +50,6 @@ class LoginViewController: UIViewController {
     
     @IBAction private func openRegisterView() {
             self.performSegue(withIdentifier: "openRegisterView", sender: nil)
+            print("openRegisterView")
         }
 }
